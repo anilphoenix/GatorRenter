6284b34a44e5d09cc1cbbc915f695c301801e797
fdai4856 <anil.manzoor@outlook.com>
2016-12-21 10:27:25 +0100

    Initial Commit for GitHub

---
+++ GatorRenter/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  GatorRenter
+//
+//  Created by fdai4856 on 21/12/2016.
+//  Copyright © 2016 fdai4856. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print("Change for initial commit to GitHub!")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
